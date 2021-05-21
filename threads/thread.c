9bea5b52be32e04e03b794798821a8784acb239d
JorgePrez <jorge_operez@yahoo.com>
2021-05-21 05:40:17 -0400

    inicializando lists de thread

--- threads/thread.c
+++ threads/thread.c
@@ -584,6 +584,11 @@ init_thread (struct thread *t, const char *name, int priority)
   
   t->magic = THREAD_MAGIC;
 
+  list_init(&t->holdingLocks);
+  list_init(&t->locksTryAcquire); 
+   
+
+
   old_level = intr_disable ();
   list_push_back (&all_list, &t->allelem);
   intr_set_level (old_level);
