f1361a7463c0ed3c6c960b7c8c8726b5b9a5a711
JorgePrez <jorge_operez@yahoo.com>
2021-05-30 06:09:52 -0400

    Operaciones basicas en FixedPoint.h

---
+++ threads/fixed-point.h
@@ -0,0 +1,23 @@
+
+#define F (1 << 14)
+
+int suma(int a, int b){
+	return a + b;
+}
+
+int resta (int a,int b){
+	return a - b;
+}
+
+int mult(int a, int b)
+{
+	return a * b;
+}
+
+
+int div(int a, int b)
+{
+	return a / b;
+}
+
+
