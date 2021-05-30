11c8b5cc59f2331e893791c8bce35095b67128a8
JorgePrez <jorge_operez@yahoo.com>
2021-05-30 05:37:45 -0400

    Set and get Nice

--- threads/thread.c
+++ threads/thread.c
@@ -404,15 +404,15 @@ thread_get_priority (void)
 void
 thread_set_nice (int nice UNUSED) 
 {
-  /* Not yet implemented. */
+   thread_current->nice = nice;
+
 }
 
 /* Returns the current thread's nice value. */
 int
 thread_get_nice (void) 
 {
-  /* Not yet implemented. */
-  return 0;
+    return thread_current()->nice;
 }
 
 /* Returns 100 times the system load average. */
